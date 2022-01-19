fc36ce13ccc3832707c9d361cb6c44393b6254b2
Nathan Ferracini <nathanferracini@gmail.com>
2022-01-18 23:01:13 -0300

    Add ci profile to execute tests with hsqldb

--- src/test/java/com/example/mocriservices/microservices/MainControllerTest.java
+++ src/test/java/com/example/mocriservices/microservices/MainControllerTest.java
@@ -7,10 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("ci")
 public class MainControllerTest {
 
     @LocalServerPort
